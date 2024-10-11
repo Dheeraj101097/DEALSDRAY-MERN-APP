97ad9a13f84521f380e258469c3be4876a89a30d
Dheeraj059 <devishree1005@gmail.com>
2024-10-11 09:42:42 +0530

    Update DashBoard.jsx

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,23 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { Link, useNavigate } from "react-router-dom";
-import {  useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Button from '@mui/material/Button';
-import {  handleSuccess } from "../utils";
 
 const DashBord = () => {
-  const [setLoggedInUser]=useState('');
-  const navigate = useNavigate();
-  useEffect(()=>{setLoggedInUser(localStorage.getItem('loggedInUser'))},[])
   
-  const handleLogout=(e)=>{
-    localStorage.removeItem('token')
-    localStorage.removeItem('loggedInUser')
-    handleSuccess("user Logged Out")
-    setTimeout(()=>{
-      navigate('/login')
-    },1000)
-  }
   let [name, setname] = useState("")
   let ID = useParams()
 
@@ -38,7 +25,7 @@ const DashBord = () => {
           <li><Button variant="text"><Link to='/create-employee'> Create Employee</Link></Button> </li>
           <li><Button variant="text"><Link to="/employee-list">  Employee list </Link></Button> </li>
           <li className='p-2 text-red-500 border border-dashed border-red-400 '>{name}</li>
-          <button onClick={handleLogout}>Logout</button>
+          <button>Logout</button>
         </ul>
       </div>
       <h1 className='bg-yellow-200 place-content-center'>DashBord</h1>
